dd29ed4dbafefb8d0ef84be2a568b54dd6a2cdf7
Thomas Freeman <thomasin500@aim.com>
2020-03-05 17:48:25 -0700

    move css files to public dir, change express imports to serve static files

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const app = require('express')(); //init nodeJS express framework
+const express = require('express');
+const app = express(); //init nodeJS express framework
 const http = require('http').createServer(app); 
-const io = require('socket.io')(http); //init the socket framework and point it to the server
+const io = require('socket.io').listen(http); //init the socket framework and point it to the server
+
+app.use(express.static('public')); //this allowed the static files in the public folder to be served
 
 app.get('/', function (request, response) {
     response.sendFile(__dirname + '/index.html'); //return the local base html file
